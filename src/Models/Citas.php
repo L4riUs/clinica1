90107f867a0072f706cde318e735b2634733cd9d
José Guillermo Pérez Soto <joseguillermojose01@gmail.com>
2025-04-17 22:32:56 -0400

    update models

--- src/Models/Citas.php
+++ src/Models/Citas.php
@@ -1,141 +1,275 @@
 <?php
     namespace Proyecto\Clinica\Models;
 
-    class Citas extends conexion{
+    // class Citas extends conexion{
 
-        private $id;
-        private $id_paciente;
-        private $id_doctor;
-        private $motivo;
-        private $precio;
-        private $fecha;
-        private $emergencia;
+    //     private $id;
+    //     private $id_paciente;
+    //     private $id_doctor;
+    //     private $motivo;
+    //     private $precio;
+    //     private $fecha;
+    //     private $emergencia;
         
-        function __construct($id=null,$id_paciente=null,$id_doctor=null,$motivo=null,$precio=null,$fecha=null,$emergencia=null){
+    //     function __construct($id=null,$id_paciente=null,$id_doctor=null,$motivo=null,$precio=null,$fecha=null,$emergencia=null){
+    //         parent::__construct();
+
+    //         $this->id = $id;
+    //         $this->id_paciente = $id_paciente;
+    //         $this->id_doctor = $id_doctor;
+    //         $this->motivo = $motivo;
+    //         $this->precio = $precio;
+    //         $this->fecha = $fecha;
+    //         $this->emergencia = $emergencia;
+    //     }
+
+    //     public function getId(){
+    //         return $this->id;
+    //     }
+
+    //     public function getIdPaciente(){
+    //         return $this->id_paciente;
+    //     }
+
+    //     public function getIdDoctor(){
+    //         return $this->id_doctor;
+    //     }
+
+    //     public function getMotivo(){
+    //         return $this->motivo;
+    //     }
+
+    //     public function getPrecio(){
+    //         return $this->precio;
+    //     }
+
+    //     public function getFecha(){
+    //         return $this->fecha;
+    //     }
+
+    //     public function setId($id){
+    //         $this->id = $id;
+    //     }
+
+    //     public function setIdPaciente($id_paciente){
+    //         $this->id_paciente = $id_paciente;
+    //     }
+
+    //     public function setIdDoctor($id_doctor){
+    //         $this->id_doctor = $id_doctor;
+    //     }
+
+    //     public function setMotivo($motivo){
+    //         $this->motivo = $motivo;
+    //     }
+
+    //     public function setPrecio($precio){
+    //         $this->precio = $precio;
+    //     }
+
+    //     public function setFecha($fecha){
+    //         $this->fecha = $fecha;
+    //     }
+
+    //     public function setEmergencia($emergencia){
+    //         $this->emergencia = $emergencia;
+    //     }
+        
+    //     public function insertar(){
+    //         $sql = "INSERT INTO citas (id_paciente, id_doctor, motivo, precio, fecha, emergencia) VALUES (:id_paciente, :id_doctor, :motivo, :precio, :fecha, :emergencia)";
+    //         $query = $this->conexion->prepare($sql);
+    //         $query->execute(array(
+    //             ':id_paciente' => $this->id_paciente,
+    //             ':id_doctor' => $this->id_doctor,
+    //             ':motivo' => $this->motivo,
+    //             ':precio' => $this->precio,
+    //             ':fecha' => $this->fecha,
+    //             ':emergencia' => $this->emergencia,
+    //         ));
+    //         $this->id = $this->conexion->lastInsertId();
+    //         return $this->id;
+    //     }
+
+    //     public function actualizar(){
+    //         $sql = "UPDATE citas SET id_paciente = :id_paciente, id_doctor = :id_doctor, motivo = :motivo, precio = :precio, fecha = :fecha, emergencia = :emergencia WHERE id = :id";
+    //         $query = $this->conexion->prepare($sql);
+    //         $query->execute(array(
+    //             ':id_paciente' => $this->id_paciente,
+    //             ':id_doctor' => $this->id_doctor,
+    //             ':motivo' => $this->motivo,
+    //             ':precio' => $this->precio,
+    //             ':fecha' => $this->fecha,
+    //             ':emergencia' => $this->emergencia,
+    //             ':id' => $this->id
+    //         ));
+    //     }
+
+    //     public function eliminar(){
+    //         $sql = "DELETE FROM citas WHERE id = :id";
+    //         $query = $this->conexion->prepare($sql);
+    //         $query->execute(array(
+    //             ':id' => $this->id
+    //         ));
+    //     }
+
+    //     public function getCitas(){
+    //         $sql = "SELECT 
+    //         citas.id, 
+    //         citas.id_paciente, 
+    //         citas.id_doctor, 
+    //         citas.motivo, 
+    //         citas.precio, 
+    //         citas.fecha, 
+    //         citas.emergencia,
+    //         paciente.nombre as paciente,
+    //         doctores.nombre as doctor
+    //         FROM citas 
+    //         INNER JOIN paciente ON citas.id_paciente = paciente.id 
+    //         INNER JOIN doctores ON citas.id_doctor = doctores.id where 1";
+    //         if (isset($this->id)) {
+    //             $sql .= " AND citas.id = :id";
+    //         }
+    //         $query = $this->conexion->prepare($sql);
+    //         $opciones = array();
+    //         if (isset($this->id)) {
+    //             $opciones[':id'] = $this->id;
+    //         }
+    //         $query->execute($opciones);
+    //         $citas = $query->fetchAll();
+    //         return $citas;
+    //     }
+    // }
+    class Citas extends Conexion {
+    
+        private $id;
+        private $id_servicio_medico;
+        private $id_paciente;
+        private $fecha;
+        private $hora;
+        private $estado;
+    
+        function __construct($id = null, $id_servicio_medico = null, $id_paciente = null, $fecha = null, $hora = null, $estado = null) {
             parent::__construct();
-
+    
             $this->id = $id;
+            $this->id_servicio_medico = $id_servicio_medico;
             $this->id_paciente = $id_paciente;
-            $this->id_doctor = $id_doctor;
-            $this->motivo = $motivo;
-            $this->precio = $precio;
             $this->fecha = $fecha;
-            $this->emergencia = $emergencia;
+            $this->hora = $hora;
+            $this->estado = $estado;
         }
-
-        public function getId(){
+    
+        // getters
+        public function getId() {
             return $this->id;
         }
-
-        public function getIdPaciente(){
+    
+        public function getIdServicioMedico() {
+            return $this->id_servicio_medico;
+        }
+    
+        public function getIdPaciente() {
             return $this->id_paciente;
         }
-
-        public function getIdDoctor(){
-            return $this->id_doctor;
-        }
-
-        public function getMotivo(){
-            return $this->motivo;
-        }
-
-        public function getPrecio(){
-            return $this->precio;
-        }
-
-        public function getFecha(){
+    
+        public function getFecha() {
             return $this->fecha;
         }
-
-        public function setId($id){
+    
+        public function getHora() {
+            return $this->hora;
+        }
+    
+        public function getEstado() {
+            return $this->estado;
+        }
+    
+        // setters
+        public function setId($id) {
             $this->id = $id;
         }
-
-        public function setIdPaciente($id_paciente){
+    
+        public function setIdServicioMedico($id_servicio_medico) {
+            $this->id_servicio_medico = $id_servicio_medico;
+        }
+    
+        public function setIdPaciente($id_paciente) {
             $this->id_paciente = $id_paciente;
         }
-
-        public function setIdDoctor($id_doctor){
-            $this->id_doctor = $id_doctor;
-        }
-
-        public function setMotivo($motivo){
-            $this->motivo = $motivo;
-        }
-
-        public function setPrecio($precio){
-            $this->precio = $precio;
-        }
-
-        public function setFecha($fecha){
+    
+        public function setFecha($fecha) {
             $this->fecha = $fecha;
         }
-
-        public function setEmergencia($emergencia){
-            $this->emergencia = $emergencia;
+    
+        public function setHora($hora) {
+            $this->hora = $hora;
         }
-        
-        public function insertar(){
-            $sql = "INSERT INTO citas (id_paciente, id_doctor, motivo, precio, fecha, emergencia) VALUES (:id_paciente, :id_doctor, :motivo, :precio, :fecha, :emergencia)";
+    
+        public function setEstado($estado) {
+            $this->estado = $estado;
+        }
+    
+        // CRUD
+        public function insertar() {
+            $sql = "INSERT INTO citas (id_servicio_medico, id_paciente, fecha, hora, estado) 
+                    VALUES (:id_servicio_medico, :id_paciente, :fecha, :hora, :estado)";
             $query = $this->conexion->prepare($sql);
             $query->execute(array(
+                ':id_servicio_medico' => $this->id_servicio_medico,
                 ':id_paciente' => $this->id_paciente,
-                ':id_doctor' => $this->id_doctor,
-                ':motivo' => $this->motivo,
-                ':precio' => $this->precio,
                 ':fecha' => $this->fecha,
-                ':emergencia' => $this->emergencia,
+                ':hora' => $this->hora,
+                ':estado' => $this->estado
             ));
             $this->id = $this->conexion->lastInsertId();
             return $this->id;
         }
-
-        public function actualizar(){
-            $sql = "UPDATE citas SET id_paciente = :id_paciente, id_doctor = :id_doctor, motivo = :motivo, precio = :precio, fecha = :fecha, emergencia = :emergencia WHERE id = :id";
+    
+        public function actualizar() {
+            $sql = "UPDATE citas 
+                    SET id_servicio_medico = :id_servicio_medico, id_paciente = :id_paciente, fecha = :fecha, hora = :hora, estado = :estado 
+                    WHERE id = :id";
             $query = $this->conexion->prepare($sql);
             $query->execute(array(
+                ':id_servicio_medico' => $this->id_servicio_medico,
                 ':id_paciente' => $this->id_paciente,
-                ':id_doctor' => $this->id_doctor,
-                ':motivo' => $this->motivo,
-                ':precio' => $this->precio,
                 ':fecha' => $this->fecha,
-                ':emergencia' => $this->emergencia,
+                ':hora' => $this->hora,
+                ':estado' => $this->estado,
                 ':id' => $this->id
             ));
         }
-
-        public function eliminar(){
+    
+        public function eliminar() {
             $sql = "DELETE FROM citas WHERE id = :id";
             $query = $this->conexion->prepare($sql);
-            $query->execute(array(
-                ':id' => $this->id
-            ));
+            $query->execute(array(':id' => $this->id));
         }
-
-        public function getCitas(){
+    
+        public function getCitas() {
             $sql = "SELECT 
-            citas.id, 
-            citas.id_paciente, 
-            citas.id_doctor, 
-            citas.motivo, 
-            citas.precio, 
-            citas.fecha, 
-            citas.emergencia,
-            paciente.nombre as paciente,
-            doctores.nombre as doctor
-            FROM citas 
-            INNER JOIN paciente ON citas.id_paciente = paciente.id 
-            INNER JOIN doctores ON citas.id_doctor = doctores.id where 1";
+                        citas.*, 
+                        paciente.nombre AS paciente,
+                        servicios.nombre AS servicio
+                    FROM citas 
+                    INNER JOIN paciente ON citas.id_paciente = paciente.id 
+                    INNER JOIN servicios ON citas.id_servicio_medico = servicios.id 
+                    WHERE 1";
+    
             if (isset($this->id)) {
                 $sql .= " AND citas.id = :id";
             }
+    
             $query = $this->conexion->prepare($sql);
             $opciones = array();
+    
             if (isset($this->id)) {
                 $opciones[':id'] = $this->id;
             }
+    
             $query->execute($opciones);
-            $citas = $query->fetchAll();
-            return $citas;
+            return $query->fetchAll();
         }
     }
+    ?>
+    
--- src/Models/Insumos.php
+++ src/Models/Insumos.php
@@ -1,84 +1,114 @@
 <?php
-   namespace Proyecto\Clinica\Models;
-    class Insumos extends Conexion{
+namespace Proyecto\Clinica\Models;
 
-        private $id;
-        private $nombre;
-        private $stock;
+class Insumos extends Conexion {
 
-        function __construct($id=null, $nombre=null, $stock=null){
-            parent::__construct();
+    private $id;
+    private $nombre;
+    private $descripcion;
+    private $precio;
+    private $estado;
 
-            $this->id = $id;
-            $this->nombre = $nombre;
-            $this->stock = $stock;
-        }
+    function __construct($id = null, $nombre = null, $descripcion = null, $precio = null, $estado = null) {
+        parent::__construct();
 
-        public function getId(){
-            return $this->id;
-        }
-
-        public function getNombre(){
-            return $this->nombre;
-        }
-
-        public function getStock(){
-            return $this->stock;
-        }
-
-        public function setId($id){
-            $this->id = $id;
-        }
-
-        public function setNombre($nombre){
-            $this->nombre = $nombre;
-        }
-
-        public function setStock($stock){
-            $this->stock = $stock;
-        }
-        
-        public function insertar(){
-            $sql = "INSERT INTO insumos (nombre, stock) VALUES (:nombre, :stock)";
-            $query = $this->conexion->prepare($sql);
-            $query->execute(array(
-                ':nombre' => $this->nombre,
-                ':stock' => $this->stock
-            ));
-            $this->id = $this->conexion->lastInsertId();
-            return $this->id;
-        }
-
-        public function actualizar(){
-            $sql = "UPDATE insumos SET nombre = :nombre, stock = :stock WHERE id = :id";
-            $query = $this->conexion->prepare($sql);
-            $query->execute(array(
-                ':nombre' => $this->nombre,
-                ':stock' => $this->stock,
-                ':id' => $this->id
-            ));
-        }
-
-        public function eliminar(){
-            $sql = "DELETE FROM insumos WHERE id = :id";
-            $query = $this->conexion->prepare($sql);
-            $query->execute(array(
-                ':id' => $this->id
-            ));
-        }
-
-        public function getInsumos(){
-            $sql = "SELECT * FROM insumos where 1";
-            if (isset($this->id)) {
-                $sql .= " AND id = :id";
-            }
-            $query = $this->conexion->prepare($sql);
-            $opciones = array();
-            if (isset($this->id)) {
-                $opciones[':id'] = $this->id;
-            }
-            $query->execute($opciones);
-            $insumos = $query->fetchAll();
-            return $insumos;
-        }
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->precio = $precio;
+        $this->estado = $estado;
     }
+
+    // getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    // setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
+
+    public function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    // CRUD
+    public function insertar() {
+        $sql = "INSERT INTO insumos (nombre, descripcion, precio, estado) 
+                VALUES (:nombre, :descripcion, :precio, :estado)";
+        $query = $this->conexion->prepare($sql);
+        $query->execute(array(
+            ':nombre' => $this->nombre,
+            ':descripcion' => $this->descripcion,
+            ':precio' => $this->precio,
+            ':estado' => $this->estado
+        ));
+        $this->id = $this->conexion->lastInsertId();
+        return $this->id;
+    }
+
+    public function actualizar() {
+        $sql = "UPDATE insumos 
+                SET nombre = :nombre, descripcion = :descripcion, precio = :precio, estado = :estado 
+                WHERE id = :id";
+        $query = $this->conexion->prepare($sql);
+        $query->execute(array(
+            ':nombre' => $this->nombre,
+            ':descripcion' => $this->descripcion,
+            ':precio' => $this->precio,
+            ':estado' => $this->estado,
+            ':id' => $this->id
+        ));
+    }
+
+    public function eliminar() {
+        $sql = "DELETE FROM insumos WHERE id = :id";
+        $query = $this->conexion->prepare($sql);
+        $query->execute(array(':id' => $this->id));
+    }
+
+    public function getInsumos() {
+        $sql = "SELECT * FROM insumos WHERE 1";
+        if (isset($this->id)) {
+            $sql .= " AND id = :id";
+        }
+        $query = $this->conexion->prepare($sql);
+        $opciones = array();
+        if (isset($this->id)) {
+            $opciones[':id'] = $this->id;
+        }
+        $query->execute($opciones);
+        $insumos = $query->fetchAll();
+        return $insumos;
+    }
+}
+?>
